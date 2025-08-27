f5d21bc04ed464b28c404c7b064e25de2b2ba7a4
ichosedub <adrien.talbot001@albright.edu>
2025-08-27 09:38:58 -0400

    first commit

---
+++ USOPPK40.py
@@ -0,0 +1,9 @@
+y = 3
+x = 7 + y
+print(x)
+greeting = "100"
+print(greeting.isdecimal())
+temperture = 100
+if not (temperture > 100):
+    print ("it's not really hot outside")
+print  ("it's a great day") 
